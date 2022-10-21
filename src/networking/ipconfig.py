824072136961abddfec5aafbd52ec1e70d7bcca3
soberz-fld <69506693+soberz-fld@users.noreply.github.com>
2022-10-21 12:23:25 +0200

    Update ipconfig.py

--- src/networking/ipconfig.py
+++ src/networking/ipconfig.py
@@ -9,3 +9,12 @@ def get_broadcast_of_devices_class_c_ip() -> str:
     s.close()
     ip_blocks = str(ip_address).split('.')
     return ip_blocks[0] + '.' + ip_blocks[1] + '.' + ip_blocks[2] + '.' + str(255)
+
+def get_ip_of_device() -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # connect to any target website
+    s.connect(('10.0.0.1', 0))
+    ip_address = s.getsockname()[0]
+    s.close()
+    ip_blocks = str(ip_address).split('.')
+    return ip_blocks[0] + '.' + ip_blocks[1] + '.' + ip_blocks[2] + '.' + ip_blocks[3]
