9ad7b541edd4743850a2205abd08a5939778f487
soberz-fld <69506693+soberz-fld@users.noreply.github.com>
2024-02-20 22:18:56 +0100

    Update mariadb_connector.py

--- src/connectors/mariadb_connector.py
+++ src/connectors/mariadb_connector.py
@@ -21,9 +21,12 @@ class MariaDBConnector:
             log(critical=str(e))
             sys.exit()
 
-    def execute_statement(self, statement: str, parameter: tuple = None) -> list:
+    def execute_statement(self, statement: str, parameter: tuple = None, return_column_headers: bool = False) -> list:
         self._cursor.execute(statement, parameter)
         result = self._cursor.fetchall()
+        if return_column_headers:
+            columns = tuple([desc[0] for desc in self._cursor.description])
+            result.insert(0, columns)
         if result:
             return result
         else:
